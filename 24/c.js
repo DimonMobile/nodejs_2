b629eabe4c5d1c9cb5cce24491ea81d4157c1640
Dimon Mobile <mobik.dimka@gmail.com>
2020-05-31 14:58:18 +0300

    crypto: first task completed

--- 24/c.js
+++ 24/c.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const crypto = require('crypto');
+const fs = require('fs');
 
-let dh = crypto.createDiffieHellman(1024, 32);
+let dh = crypto.createDiffieHellman(256, 32);
 let p = dh.getPrime();
 let g = dh.getGenerator();
 let A = dh.generateKeys();
@@ -11,6 +12,31 @@ let requestObject = {
     g: g.toString('hex'),
     A: A.toString('hex')
 };
+
+function sendNext(secret) {
+    http.request({
+        method: 'get',
+        host: 'localhost',
+        port: 3000,
+        path: '/resource'
+    }, res => {
+        let ws = fs.createWriteStream('./downloaded.txt');
+        let cipheredData = '';
+        res.on('data', chunk => {
+            cipheredData += chunk;
+        });
+
+        res.on('end', () => {
+            let obj = JSON.parse(cipheredData);
+            let iv = Buffer.from(obj.iv, 'base64');
+            let data = Buffer.from(obj.data, 'base64');
+            let decipheriv = crypto.createDecipheriv('aes-256-cbc', secret, iv);
+            decipheriv.update(data);
+            ws.end(decipheriv.final());
+        });
+    }).end();
+}
+
 let request = JSON.stringify(requestObject);
 let reqObject = http.request(
     {
@@ -29,6 +55,7 @@ let reqObject = http.request(
             let B = Buffer.from(JSON.parse(collectedData).B, 'hex');
             let secret = dh.computeSecret(B);
             console.log(secret.toString('hex'));
+            sendNext(secret);
         });
     }
 );
--- 24/s.js
+++ 24/s.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const crypto = require('crypto');
 const app = express();
+const fs = require('fs');
+
+let secret = null;
 
 app.post('/generate', (req, res, next) => {
     let collectedData = '';
@@ -14,7 +17,7 @@ app.post('/generate', (req, res, next) => {
 
         let dh = crypto.createDiffieHellman(Buffer.from(parsed.p, 'hex'), Buffer.from(parsed.g, 'hex'));
         let B = dh.generateKeys();
-        let secret = dh.computeSecret(Buffer.from(parsed.A, 'hex'));
+        secret = dh.computeSecret(Buffer.from(parsed.A, 'hex'));
         console.log(secret.toString('hex'));
 
         res.type('json');
@@ -22,6 +25,17 @@ app.post('/generate', (req, res, next) => {
             B: B.toString('hex')
         }));
     });
+});
+
+app.get('/resource', (req, res, next) => {
+    const iv = crypto.randomBytes(16);
+    let cipheriv = crypto.createCipheriv('aes-256-cbc', secret, iv);
+    let readedData = fs.readFileSync('./host.txt');
+    cipheriv.update(Buffer.from(readedData, 'binary'));
+    res.end(JSON.stringify({
+        iv: iv.toString('base64'),
+        data: cipheriv.final().toString('base64')
+    }));
 });
 
 app.listen(3000);
